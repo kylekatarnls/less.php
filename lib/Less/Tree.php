e0bda71b87703aecb11cf13db591719913a51cf4
Kyle <kylekatarnls@gmail.com>
2017-08-06 13:26:58 +0000

    Apply fixes from StyleCI

--- lib/Less/Tree.php
+++ lib/Less/Tree.php
@@ -1,90 +1,89 @@
 <?php
 
 /**
- * Tree
- *
- * @package Less
- * @subpackage tree
+ * Tree.
  */
-class Less_Tree{
+class Less_Tree
+{
+    public $cache_string;
 
-	public $cache_string;
+    public function toCSS()
+    {
+        $output = new Less_Output();
+        $this->genCSS($output);
 
-	public function toCSS(){
-		$output = new Less_Output();
-		$this->genCSS($output);
-		return $output->toString();
-	}
-
+        return $output->toString();
+    }
 
     /**
-     * Generate CSS by adding it to the output object
+     * Generate CSS by adding it to the output object.
      *
      * @param Less_Output $output The output
+     *
      * @return void
      */
-    public function genCSS($output){}
+    public function genCSS($output)
+    {
+    }
 
+    /**
+     * @param Less_Tree_Ruleset[] $rules
+     */
+    public static function outputRuleset($output, $rules)
+    {
+        $ruleCnt = count($rules);
+        Less_Environment::$tabLevel++;
 
-	/**
-	 * @param Less_Tree_Ruleset[] $rules
-	 */
-	public static function outputRuleset( $output, $rules ){
+        // Compressed
+        if (Less_Parser::$options['compress']) {
+            $output->add('{');
+            for ($i = 0; $i < $ruleCnt; $i++) {
+                $rules[$i]->genCSS($output);
+            }
 
-		$ruleCnt = count($rules);
-		Less_Environment::$tabLevel++;
+            $output->add('}');
+            Less_Environment::$tabLevel--;
 
+            return;
+        }
 
-		// Compressed
-		if( Less_Parser::$options['compress'] ){
-			$output->add('{');
-			for( $i = 0; $i < $ruleCnt; $i++ ){
-				$rules[$i]->genCSS( $output );
-			}
+        // Non-compressed
+        $tabSetStr = "\n".str_repeat(Less_Parser::$options['indentation'], Less_Environment::$tabLevel - 1);
+        $tabRuleStr = $tabSetStr.Less_Parser::$options['indentation'];
 
-			$output->add( '}' );
-			Less_Environment::$tabLevel--;
-			return;
-		}
+        $output->add(' {');
+        for ($i = 0; $i < $ruleCnt; $i++) {
+            $output->add($tabRuleStr);
+            $rules[$i]->genCSS($output);
+        }
+        Less_Environment::$tabLevel--;
+        $output->add($tabSetStr.'}');
+    }
 
+    public function accept($visitor)
+    {
+    }
 
-		// Non-compressed
-		$tabSetStr = "\n".str_repeat( Less_Parser::$options['indentation'] , Less_Environment::$tabLevel-1 );
-		$tabRuleStr = $tabSetStr.Less_Parser::$options['indentation'];
+    public static function ReferencedArray($rules)
+    {
+        foreach ($rules as $rule) {
+            if (method_exists($rule, 'markReferenced')) {
+                $rule->markReferenced();
+            }
+        }
+    }
 
-		$output->add( " {" );
-		for($i = 0; $i < $ruleCnt; $i++ ){
-			$output->add( $tabRuleStr );
-			$rules[$i]->genCSS( $output );
-		}
-		Less_Environment::$tabLevel--;
-		$output->add( $tabSetStr.'}' );
+    /**
+     * Requires php 5.3+.
+     */
+    public static function __set_state($args)
+    {
+        $class = get_called_class();
+        $obj = new $class(null, null, null, null);
+        foreach ($args as $key => $val) {
+            $obj->$key = $val;
+        }
 
-	}
-
-	public function accept($visitor){}
-
-
-	public static function ReferencedArray($rules){
-		foreach($rules as $rule){
-			if( method_exists($rule, 'markReferenced') ){
-				$rule->markReferenced();
-			}
-		}
-	}
-
-
-	/**
-	 * Requires php 5.3+
-	 */
-	public static function __set_state($args){
-
-		$class = get_called_class();
-		$obj = new $class(null,null,null,null);
-		foreach($args as $key => $val){
-			$obj->$key = $val;
-		}
-		return $obj;
-	}
-
+        return $obj;
+    }
 }
