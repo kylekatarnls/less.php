10d8844c93975b64919a8e9b1de81f34a16e845a
KyleKatarn <kylekatarnls@gmail.com>
2017-09-22 10:30:09 +0200

    Fix PHP 5.3 unit tests compatibility

--- test/phpunit/SyntaxTest.php
+++ test/phpunit/SyntaxTest.php
@@ -10,15 +10,15 @@ class phpunit_SyntaxTest extends phpunit_bootstrap
         echo "\nSyntax Tests";
 
         $parser = new Less_Parser();
-        $parser->parseFile(__DIR__.'/../Fixtures/variables/style.less');
+        $parser->parseFile(__DIR__ . '/../Fixtures/variables/style.less');
         $css = $parser->getCss();
 
-        $this->assertEquals(implode("\n", [
+        $this->assertEquals(implode("\n", array(
             'body {',
             '  color: #ff0000;',
             '}',
             ''
-        ]), $css);
+        )), $css);
     }
 
     /**
@@ -29,15 +29,15 @@ class phpunit_SyntaxTest extends phpunit_bootstrap
         echo "\nSyntax Tests";
 
         $parser = new Less_Parser();
-        $parser->parseFile(__DIR__.'/../Fixtures/math/style.less');
+        $parser->parseFile(__DIR__ . '/../Fixtures/math/style.less');
         $css = $parser->getCss();
 
-        $this->assertEquals(implode("\n", [
+        $this->assertEquals(implode("\n", array(
             '#foo {',
             '  font-size: 13.07692308rem;',
             '  line-height: 13.84615385rem;',
             '}',
             '',
-        ]), $css);
+        )), $css);
     }
 }
