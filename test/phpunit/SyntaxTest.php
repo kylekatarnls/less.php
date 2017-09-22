3bde10687f8f688d2ff313b440fbdf0742cac3de
KyleKatarn <kylekatarnls@gmail.com>
2017-09-22 09:29:41 +0200

    Add some unit tests for variables propagation and math

---
+++ test/phpunit/SyntaxTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class phpunit_SyntaxTest extends phpunit_bootstrap
+{
+    /**
+     * Test.
+     */
+    public function testVariablesInImport()
+    {
+        echo "\nSyntax Tests";
+
+        $parser = new Less_Parser();
+        $parser->parseFile(__DIR__.'/../Fixtures/variables/style.less');
+        $css = $parser->getCss();
+
+        $this->assertEquals(implode("\n", [
+            'body {',
+            '  color: #ff0000;',
+            '}',
+            ''
+        ]), $css);
+    }
+
+    /**
+     * Test.
+     */
+    public function testMath()
+    {
+        echo "\nSyntax Tests";
+
+        $parser = new Less_Parser();
+        $parser->parseFile(__DIR__.'/../Fixtures/math/style.less');
+        $css = $parser->getCss();
+
+        $this->assertEquals(implode("\n", [
+            '#foo {',
+            '  font-size: 13.07692308rem;',
+            '  line-height: 13.84615385rem;',
+            '}',
+            '',
+        ]), $css);
+    }
+}
